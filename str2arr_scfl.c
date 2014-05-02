48343036503d192b3ede3a0b6e28bc4aa583fe6f
Ramonf <ramonfallon@gmail.com>
2014-05-03 00:19:37 +0200

    long time no commit: problems with the str2arr .. discovered the realloc is difficult (need a DOUBLE TWIN). Solved now

--- str2arr_scfl.c
+++ str2arr_scfl.c
@@ -49,6 +49,22 @@
         memset((a)+(b)-(c), 0, (c)*sizeof(t)); \
     }
 
+/* quick macro for conditionally enlarging a TWO twin digit and char array based on same lnbuf, a2 MUST be a double char ptr */
+#define CONDREALLOTDCA2(x, b, c, a, a1, a2, a3, t, i, strbuf); \
+    if((x)==((b)-1)) { \
+        (b) += (c); \
+        (a)=realloc((a), (b)*sizeof(t)); \
+        (a1)=realloc((a1), (b)*sizeof(t)); \
+        (a2)=realloc((a2), (b)*sizeof(char*)); \
+        (a3)=realloc((a3), (b)*sizeof(char*)); \
+        for((i)=(b)-(c);(i)<(b);++(i)) { \
+        (a2)[(i)]=malloc((strbuf)*sizeof(char)); \
+        (a3)[(i)]=malloc((strbuf)*sizeof(char)); \
+        } \
+        memset((a)+(b)-(c), 0, (c)*sizeof(t)); \
+        memset((a1)+(b)-(c), 0, (c)*sizeof(t)); \
+    }
+
 typedef struct
 {
     char *fl;
@@ -69,12 +85,14 @@ void f2flpua_t(char *fname, flpua_t **lnarr_p)
         if(c == EOF) break;
         if(c == '\n') {
             if(lidx !=0) {
-                CONDREALLOTDCA(lidx-1, lnbuf, LNBUF, (*lnarr_p)->ua1, (*lnarr_p)->stra1, unsigned, j, GSTRBUF);
+                CONDREALLOTDCA2(lidx-1, lnbuf, LNBUF, (*lnarr_p)->ua0, (*lnarr_p)->ua1, (*lnarr_p)->stra0, (*lnarr_p)->stra1, unsigned, j, GSTRBUF);
                 (*lnarr_p)->ua1[lidx-1]=lnsz;
                 (*lnarr_p)->stra1[lidx-1][lnsz]='\0';
+                (*lnarr_p)->stra1[lidx-1]=realloc((*lnarr_p)->stra1[lidx-1], ((*lnarr_p)->ua1[lidx-1]+1)*sizeof(char));
             } else {
                 (*lnarr_p)->fl[lnsz]='\0';
                 (*lnarr_p)->flsz=lnsz;
+                (*lnarr_p)->fl=realloc((*lnarr_p)->fl, ((*lnarr_p)->flsz+1)*sizeof(char));
             }
             lidx++;
             lnsz=0;
@@ -85,11 +103,12 @@ void f2flpua_t(char *fname, flpua_t **lnarr_p)
                 CONDREALLOCP(lnsz, strbuf, GSTRBUF, (*lnarr_p)->fl);
                 (*lnarr_p)->fl[lnsz]=c;
             } else {
-                if(!seentok) {
-                    if(c==CTOK) {
-                        CONDREALLOTDCA(lidx-1, lnbuf, LNBUF, (*lnarr_p)->ua0, (*lnarr_p)->stra0, unsigned, j, GSTRBUF);
+                if(!seenctok) {
+                    if(c==CTOK) { /* finish up our "0" counterparts */
+//                        CONDREALLOTDCA(lidx-1, lnbuf, LNBUF, (*lnarr_p)->ua0, (*lnarr_p)->stra0, unsigned, j, GSTRBUF);
                         (*lnarr_p)->ua0[lidx-1]=lnsz;
                         (*lnarr_p)->stra0[lidx-1][lnsz]='\0';
+                        (*lnarr_p)->stra0[lidx-1]=realloc((*lnarr_p)->stra0[lidx-1], ((*lnarr_p)->ua0[lidx-1]+1)*sizeof(char));
                         lnsz=0;
                         seenctok=1; /* toggle */
                         strbuf=GSTRBUF;
@@ -106,11 +125,14 @@ void f2flpua_t(char *fname, flpua_t **lnarr_p)
         }
     }
     (*lnarr_p)->uasz=lidx-1;
-    (*lnarr_p)->fl=realloc((*lnarr_p)->fl, ((*lnarr_p)->flsz+1)*sizeof(char)); /* the plus one is because flsz leaves out nullchar */
-    (*lnarr_p)->ua=realloc((*lnarr_p)->ua, (*lnarr_p)->uasz*sizeof(unsigned));
-    for(j=(*lnarr_p)->uasz;j<lnbuf;++j) 
-        free((*lnarr_p)->stra[j]);
-    (*lnarr_p)->stra=realloc((*lnarr_p)->stra, (*lnarr_p)->uasz*sizeof(char*));
+    (*lnarr_p)->ua0=realloc((*lnarr_p)->ua0, (*lnarr_p)->uasz*sizeof(unsigned));
+    (*lnarr_p)->ua1=realloc((*lnarr_p)->ua1, (*lnarr_p)->uasz*sizeof(unsigned));
+    for(j=(*lnarr_p)->uasz;j<lnbuf;++j) {
+        free((*lnarr_p)->stra0[j]);
+        free((*lnarr_p)->stra1[j]);
+    }
+    (*lnarr_p)->stra0=realloc((*lnarr_p)->stra0, (*lnarr_p)->uasz*sizeof(char*));
+    (*lnarr_p)->stra1=realloc((*lnarr_p)->stra1, (*lnarr_p)->uasz*sizeof(char*));
 
     fclose(fin);
     return;
@@ -128,51 +150,52 @@ flpua_t *crea_flpua_t(void) /* with minimum memory allocations as well */
     for(j=0;j<LNBUF;++j) {
         lnarr_p->stra0[j]=calloc(GSTRBUF, sizeof(char));
         lnarr_p->stra1[j]=calloc(GSTRBUF, sizeof(char));
-        return lnarr_p;
+    }
+    return lnarr_p;
+}
+
+void prto_flpua_t(flpua_t *lnarr_p)
+{
+    unsigned j;
+    printf("Input has %u lines (less first line), per line chars is (first line is excluded):\n", lnarr_p->uasz); 
+    printf("First line _string_, of length %u, is: \"%s\"\n", lnarr_p->flsz, lnarr_p->fl); 
+    printf("Listing of the rest of the lines\n"); 
+    for(j=0;j<lnarr_p->uasz;++j) 
+        printf("l%3u:\"%s%s\"(%u)\n", j, lnarr_p->stra0[j], lnarr_p->stra1[j], lnarr_p->ua0[j]+lnarr_p->ua1[j]);
+    return;
+}
+
+void free_flpua_t(flpua_t **lnarr_p)
+{
+    unsigned j;
+    free((*lnarr_p)->ua0);
+    free((*lnarr_p)->ua1);
+    free((*lnarr_p)->fl);
+    for(j=0;j<(*lnarr_p)->uasz;++j) {
+        free((*lnarr_p)->stra0[j]);
+        free((*lnarr_p)->stra1[j]);
+    }
+    free((*lnarr_p)->stra0);
+    free((*lnarr_p)->stra1);
+    free((*lnarr_p));
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    /* argument accounting: remember argc, the number of arguments, _includes_ the executable */
+    if(argc!=2) {
+        printf("Error. Pls supply argument (name of file).\n");
+        exit(EXIT_FAILURE);
     }
 
-    void prto_flpua_t(flpua_t *lnarr_p)
-    {
-        unsigned j;
-        printf("Input has %u lines (less first line), per line chars is (first line is excluded):\n", lnarr_p->uasz); 
-        printf("First line _string_, of length %u, is: \"%s\"\n", lnarr_p->flsz, lnarr_p->fl); 
-        printf("Listing of the rest of the lines\n"); 
-        for(j=0;j<lnarr_p->uasz;++j) 
-            printf("l%3u:\"%s\"(%u)\n", j, lnarr_p->stra[j], lnarr_p->ua[j]);
-        return;
-    }
+    /* convert this file into a line array */
+    flpua_t *lnarr_p=crea_flpua_t();
+    f2flpua_t(argv[1], &lnarr_p);
+    // prto_flpua_t(lnarr_p);
+    printf("ctok: %c;\n", CTOK); 
 
-    void free_flpua_t(flpua_t **lnarr_p)
-    {
-        unsigned j;
-        free((*lnarr_p)->ua0);
-        free((*lnarr_p)->ua1);
-        free((*lnarr_p)->fl);
-        for(j=0;j<(*lnarr_p)->uasz;++j) {
-            free((*lnarr_p)->stra0[j]);
-            free((*lnarr_p)->stra1[j]);
-        }
-        free((*lnarr_p)->stra0);
-        free((*lnarr_p)->stra1);
-        free((*lnarr_p));
-        return;
-    }
+    free_flpua_t(&lnarr_p);
 
-    int main(int argc, char *argv[])
-    {
-        /* argument accounting: remember argc, the number of arguments, _includes_ the executable */
-        if(argc!=2) {
-            printf("Error. Pls supply argument (name of file).\n");
-            exit(EXIT_FAILURE);
-        }
-
-        /* convert this file into a line array */
-        flpua_t *lnarr_p=crea_flpua_t();
-        f2flpua_t(argv[1], &lnarr_p);
-        // prto_flpua_t(lnarr_p);
-        printf("ctok: %c;\n", CTOK); 
-
-        free_flpua_t(&lnarr_p);
-
-        return 0;
-    }
+    return 0;
+}
