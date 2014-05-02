1d2a6bda0b3e9ecda317317e6a3ab2176dc0314e
Ramon F <ramonfallon@gmail.com>
2014-05-02 11:36:50 +0200

    a struct will also want its very pwn creation function too

--- lnarr.c
+++ lnarr.c
@@ -28,14 +28,14 @@ typedef struct
 
 void f2ua_bt(char *fname, ua_bt **lnarr_p)
 {
-   FILE *fin=fopen(fname, "r");
-   int c;
-   unsigned lnsz=0, lnbuf=LNBUF;
-   unsigned lidx=0;
-   (*lnarr_p)->ua=realloc((*lnarr_p)->ua, LNBUF*sizeof(unsigned)); /* pointer to a line array, packaged as a ptr to ua_bt */
-   memset((*lnarr_p)->ua, 0, lnbuf*sizeof(unsigned));
+    FILE *fin=fopen(fname, "r");
+    int c;
+    unsigned lnsz=0, lnbuf=LNBUF;
+    unsigned lidx=0;
+    (*lnarr_p)->ua=realloc((*lnarr_p)->ua, LNBUF*sizeof(unsigned)); /* pointer to a line array, packaged as a ptr to ua_bt */
+    memset((*lnarr_p)->ua, 0, lnbuf*sizeof(unsigned));
 
-   for(;;) {
+    for(;;) {
         c=fgetc(fin);
         if(c == EOF) break;
         if(c == '\n') {
@@ -44,43 +44,49 @@ void f2ua_bt(char *fname, ua_bt **lnarr_p)
             lnsz=0;
         } else
             lnsz++;
-   }
-   (*lnarr_p)->uasz=lidx;
-   (*lnarr_p)->ua=realloc((*lnarr_p)->ua, (*lnarr_p)->uasz*sizeof(unsigned));
+    }
+    (*lnarr_p)->uasz=lidx;
+    (*lnarr_p)->ua=realloc((*lnarr_p)->ua, (*lnarr_p)->uasz*sizeof(unsigned));
 
-   fclose(fin);
-   return;
+    fclose(fin);
+    return;
 }
 
 
+ua_bt *crea_ua_bt(void)
+{
+    ua_bt *lnarr_p=malloc(sizeof(ua_bt)); /* because it starts as NULL: pointer to a line array, packaged as a ptr to ua_bt */
+    lnarr_p->ua=NULL; /* valgrind very picky about initializations */
+    return lnarr_p;
+}
+
 void free_ua_bt(ua_bt **lnarr_p)
 {
-   free((*lnarr_p)->ua);
-   free((*lnarr_p));
-   return;
+    free((*lnarr_p)->ua);
+    free((*lnarr_p));
+    return;
 }
 
 
 int main(int argc, char *argv[])
 {
-   /* argument accounting: remember argc, the number of arguments, _includes_ the executable */
-	if(argc!=2) {
-		printf("Error. Pls supply argument (name of file).\n");
-		exit(EXIT_FAILURE);
-	}
+    /* argument accounting: remember argc, the number of arguments, _includes_ the executable */
+    if(argc!=2) {
+        printf("Error. Pls supply argument (name of file).\n");
+        exit(EXIT_FAILURE);
+    }
     unsigned j;
 
     /* convert this file into a line array */
-   ua_bt *lnarr_p=malloc(sizeof(ua_bt)); /* because it starts as NULL: pointer to a line array, packaged as a ptr to ua_bt */
-   lnarr_p->ua=NULL; /* valgrind very pocky about initiaizations */
-   f2ua_bt(argv[1], &lnarr_p);
+    ua_bt *lnarr_p=crea_ua_bt();
+    f2ua_bt(argv[1], &lnarr_p);
 
-   printf("Input has %u lines, per line chars is:\n", lnarr_p->uasz); 
-   for(j=0;j<lnarr_p->uasz;++j) 
-       printf("%u ", lnarr_p->ua[j]);
-   printf("\n"); 
+    printf("Input has %u lines, per line chars is:\n", lnarr_p->uasz); 
+    for(j=0;j<lnarr_p->uasz;++j) 
+        printf("%u ", lnarr_p->ua[j]);
+    printf("\n"); 
 
-   free_ua_bt(&lnarr_p);
+    free_ua_bt(&lnarr_p);
 
-   return 0;
+    return 0;
 }
