32d550e8fe9ff7808ac7e9e27404c72179dc69f2
Ramon F <ramonfallon@gmail.com>
2014-05-02 12:52:25 +0200

    continung with the "scan first line" variation: now treats it separately and prints it out

--- lnarr_scfl.c
+++ lnarr_scfl.c
@@ -13,6 +13,13 @@
         (a)=realloc((a), (b)*sizeof(t)); \
     }
 
+/* quick macro for conditionally enlarging a char pointer, space always available for final null char */
+#define CONDREALLOCP(x, b, c, a); \
+    if((x)==((b)-2)) { \
+        (b) += (c); \
+        (a)=realloc((a), (b)*sizeof(char)); \
+    }
+
 /* quick macro for conditionally enlarging a numeric native type array and setting extended memory to zero */
 #define CONDREALLOD(x, b, c, a, t); \
     if((x)==((b)-1)) { \
@@ -25,49 +32,61 @@ typedef struct
 {
     char *fl;
     unsigned *ua;
-    unsigned uasz;
+    unsigned uasz, flsz;
 } flpua_t; /* First Line Plus Unsigned Array Type */
 
 void f2flpua_t(char *fname, flpua_t **lnarr_p)
 {
     FILE *fin=fopen(fname, "r");
     int c;
-    unsigned lnsz=0, lnbuf=LNBUF;
-    unsigned lidx=0;
-    (*lnarr_p)->ua=realloc((*lnarr_p)->ua, LNBUF*sizeof(unsigned)); /* pointer to a line array, packaged as a ptr to flpua_t */
+    unsigned lnsz=0, lnbuf=LNBUF, strbuf=GSTRBUF, lidx=0;
+    (*lnarr_p)->ua=realloc((*lnarr_p)->ua, lnbuf*sizeof(unsigned)); /* pointer to a line array, packaged as a ptr to flpua_t */
+    (*lnarr_p)->fl=realloc((*lnarr_p)->fl, strbuf*sizeof(char));
     memset((*lnarr_p)->ua, 0, lnbuf*sizeof(unsigned));
+    memset((*lnarr_p)->fl, '\0', strbuf*sizeof(char));
 
     for(;;) {
         c=fgetc(fin);
         if(c == EOF) break;
         if(c == '\n') {
-            CONDREALLOD(lidx, lnbuf, LNBUF, (*lnarr_p)->ua, unsigned);
-            (*lnarr_p)->ua[lidx++]=lnsz;
+            if(lidx !=0) {
+                CONDREALLOD(lidx-1, lnbuf, LNBUF, (*lnarr_p)->ua, unsigned);
+                (*lnarr_p)->ua[lidx-1]=lnsz;
+            } else {
+                (*lnarr_p)->fl[lnsz]='\0';
+                (*lnarr_p)->flsz=lnsz;
+            }
+            lidx++;
             lnsz=0;
-        } else
+        } else {
+            if(lidx ==0) {
+                CONDREALLOCP(lnsz, strbuf, GSTRBUF, (*lnarr_p)->fl);
+                (*lnarr_p)->fl[lnsz]=c;
+            }
             lnsz++;
+        }
     }
-    (*lnarr_p)->uasz=lidx;
+    (*lnarr_p)->uasz=lidx-1;
+    (*lnarr_p)->fl=realloc((*lnarr_p)->fl, ((*lnarr_p)->flsz+1)*sizeof(char)); /* the plus one is because flsz leaves out nullchar */
     (*lnarr_p)->ua=realloc((*lnarr_p)->ua, (*lnarr_p)->uasz*sizeof(unsigned));
 
     fclose(fin);
     return;
 }
 
-
 flpua_t *crea_flpua_t(void)
 {
     flpua_t *lnarr_p=malloc(sizeof(flpua_t));
-    lnarr_p->fl=calloc(GSTRBUF, sizeof(char));
     lnarr_p->ua=NULL; /* valgrind very picky about initializations */
+    lnarr_p->fl=NULL;
     return lnarr_p;
 }
 
 void prto_flpua_t(flpua_t *lnarr_p)
 {
     unsigned j;
-    printf("Input has %u lines, per line chars is (first line is excluded):\n", lnarr_p->uasz); 
-    printf("First line is: \"%s\"\n", lnarr_p->fl); 
+    printf("Input has %u lines (less first line), per line chars is (first line is excluded):\n", lnarr_p->uasz); 
+    printf("First line _string_, of length %u, is: \"%s\"\n", lnarr_p->flsz, lnarr_p->fl); 
     for(j=0;j<lnarr_p->uasz;++j) 
         printf((j==lnarr_p->uasz-1)? "%u\n" : "%u ", lnarr_p->ua[j]);
     return;
